cd75bd27452a207702edf2c6ab686fbe52fb3ee1
tristonpang <t.pang288@hotmail.com>
2019-03-18 03:54:48 +0800

    update resume and remove portfolio section

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
         <Header resumeData = {resumeData}/>
         <About />
         <Resume />
-        <Portfolio />
         <Testimonials />
         <ContactUs />
         <Footer />
--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -14,25 +14,19 @@ export default class Header extends Component {
           <div className="nine columns main-col">
             <div className="row item">
               <div className="twelve columns">
-                <h3>University of Life</h3>
-                <p className="info">Master in Graphic Design <span>•</span> <em className="date">April 2007</em></p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-                  ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                  Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
-                </p>
+                <h3>National University of Singapore</h3>
+                <p className="info">Bachelor of Computing (Honours) in Computer Science <span>•</span> <em className="date">August 2017 - Present</em></p>
+                <p></p>
               </div>
             </div> {/* item end */}
             <div className="row item">
               <div className="twelve columns">
-                <h3>School of Cool Designers</h3>
-                <p className="info">B.A. Degree in Graphic Design <span>•</span> <em className="date">March 2003</em></p>
+                <h3>College of Alice & Peter Tan</h3>
+                <p className="info">University Town College Programme (UTCP) <span>•</span> <em className="date">August 2017 - Present</em></p>
                 <p>
-                  This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                  Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
-                  ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat
+                  Through its multidisciplinary curriculum, the UTCP helps students develop effective communications and strong writing skills, critical thinking skills, 
+                  and intellectual breadth and rigour to tackle the complex, inter-related challenges confronting the world today. 
+                  A host of informal learning activities, such as talks by distinguished visitors, as well as social and sporting activities, complements the formal curriculum.
                 </p>
               </div>
             </div> {/* item end */}
@@ -47,25 +41,35 @@ export default class Header extends Component {
           <div className="nine columns main-col">
             <div className="row item">
               <div className="twelve columns">
-                <h3>Awesome Design Studio</h3>
-                <p className="info">Senior UX Designer <span>•</span> <em className="date">March 2010 - Present</em></p>
+                <h3>Steady</h3>
+                <p className="info">Founder, Lead Developer <span>•</span> <em className="date">September 2018 - Present</em></p>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-                  ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                  Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
+                  Steady is a lifestyle marketplace mobile application catered to the hearing impaired. The idea was first formulated during a hackathon, 
+                  and has since developed into a social enterprise. Steady is currently a work-in-progress.
                 </p>
               </div>
             </div> {/* item end */}
             <div className="row item">
               <div className="twelve columns">
-                <h3>Super Cool Studio</h3>
-                <p className="info">UX Designer <span>•</span> <em className="date">March 2007 - February 2010</em></p>
+                <h3>NebulasTree Private Limited</h3>
+                <p className="info">Intern (Software Development) <span>•</span> <em className="date">Dec 2018 - Jan 2019</em></p>
                 <p>
-                  This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                  Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
-                  ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat
+                  NebulasTree is a small-medium enterprise with a 6 man team that specialises in software engineering and cybersecurity.
+                  In my time as an intern there, I was tasked to develop a One-Time Password (OTP) mobile authenticator system for iOS platforms.
+                  In the span of a month, I created from scratch the client-side aspect of the system, despite being unfamiliar with Swift and Xcode.
+                  I think on my feet, and I took up this job to challenge myself and learn something new in a short period of time.
+                </p>
+              </div>
+            </div> {/* item end */}
+            <div className="row item">
+              <div className="twelve columns">
+                <h3>National University of Singapore</h3>
+                <p className="info">Teaching Assistant <span>•</span> <em className="date">Jan 2018 - Present</em></p>
+                <p>
+                  I have conducted lessons for two modules in NUS: CS2030 Programming Methodology II and CS2103/T Software Engineering.
+                  Tutoring for CS2030 involved preparing lesson materials and conducting lab sessions, while in CS2103/T I was placed in charge
+                  of two student teams, and was responsible for the grading and guidance of the software engineering projects that each team was developing.
+                  Tutoring is meaningful to me, as it allows me to revise and hone my skills, as well as inspire fellow aspiring developers.
                 </p>
               </div>
             </div> {/* item end */}
@@ -78,19 +82,16 @@ export default class Header extends Component {
             <h1><span>Skills</span></h1>
           </div>
           <div className="nine columns main-col">
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-              voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt.
+            <p>
+              I specialise in Software Engineering, and have experience in Mobile Development. 
+              I consider myself a back-end developer, but can also do some front-end development as well.
             </p>
             <div className="bars">
               <ul className="skills">
-                <li><span className="bar-expand photoshop" /><em>Photoshop</em></li>
-                <li><span className="bar-expand illustrator" /><em>Illustrator</em></li>
-                <li><span className="bar-expand wordpress" /><em>Wordpress</em></li>
-                <li><span className="bar-expand css" /><em>CSS</em></li>
-                <li><span className="bar-expand html5" /><em>HTML5</em></li>
-                <li><span className="bar-expand jquery" /><em>jQuery</em></li>
+                <li><span className="bar-expand java" /><em>Java</em></li>
+                <li><span className="bar-expand python" /><em>Python</em></li>
+                <li><span className="bar-expand android" /><em>Mobile Development (Android)</em></li>
+                <li><span className="bar-expand web" /><em>Web Development</em></li>
               </ul>
             </div>{/* end skill-bars */}
           </div> {/* main-col end */}
