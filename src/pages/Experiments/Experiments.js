9048efbc438f2ba187d37a9f73d88d523ed087d1
Triston Pang <triston@span.io>
2020-06-13 21:12:37 +0800

    Add nav bar to Experiments

--- src/pages/Experiments/Experiments.js
+++ src/pages/Experiments/Experiments.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import classes from "./Experiments.module.css";
 
 const Experiments = () => {
   return (
-    <div>
-      <h1>Experiments page, nav works</h1>
-    </div>
-  )
-}
+    <>
+      <Navbar bg="dark" variant="dark" sticky="top" className={classes.navbar}>
+        <Navbar.Brand href='/'>triston.dev</Navbar.Brand>
+        <Nav className={classes.nav}>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/experiments">Experiments</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div className={classes.mainContentContainer}>
+        <p>Test</p>
+      </div>
+    </>
+  );
+};
 
 export default Experiments;
